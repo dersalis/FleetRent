d7376bc74cfaff0b9426b5ddf0af6337b3e2b848
damianruta <damian.ruta@hotmail.com>
2023-12-04 19:32:47 +0100

    Car model

---
+++ src/FleetRent.Api/Models/Car.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FleetRent.Api.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int ProductionYear { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int Mileage { get; set; }
+        public string Color { get; set; }
+        public FuelType FuelType { get; set; }
+
+    }
+}
